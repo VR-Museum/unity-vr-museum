cdfda5f2b216b5950c3b928c14c40474d0643e4a
Aleksey <lakhanskii@skbkontur.ru>
2022-04-20 22:34:41 +0700

    minerals attaching fix & moon and mars maps

--- Assets/src/museum/quiz/script/hardness/HardnessBrush.cs
+++ Assets/src/museum/quiz/script/hardness/HardnessBrush.cs
@@ -8,20 +8,18 @@ namespace src.museum.quiz.script.hardness
     {
         public CustomRenderTexture hardnessHeightMap;
         public Material heightMapUpdate;
-
+        [Range(0.001f, 20f)]
+        public float hardnessMultiplier = 5;
+        
         private GameObject _gameObject;
-        private Camera _mainCamera;
 
         private static readonly int ContactPosition = Shader.PropertyToID("_ContactPosition");
         private static readonly int SmoothMultiplier = Shader.PropertyToID("_smoothMultiplier");
-
-        private GameObject _lastContactedMineral = null;
         
         public void Start()
         {
             heightMapUpdate.SetVector(ContactPosition, new Vector2(-2f, -2f));
             hardnessHeightMap.Initialize();
-            _mainCamera = Camera.main;
         }
 
         private void OnCollisionStay(Collision collision)
@@ -32,39 +30,11 @@ namespace src.museum.quiz.script.hardness
                 var ray = new Ray(contact.point - contact.normal * 0.5f * 0.1f, contact.normal);
                 if (Physics.Raycast(ray, out var hit, 0.1f))
                 {
-                    // if (_lastContactedMineral != null)
-                    // {
-                        // if (collision.gameObject != _lastContactedMineral)
-                        // {
-                            // heightMapUpdate.SetVector(ContactPosition, new Vector2(-2f, -2f));
-                            // hardnessHeightMap.Initialize();
-                            // _lastContactedMineral = collision.gameObject;
-                        // }
-                    // }
-                    // else
-                    // {
-                        // _lastContactedMineral = collision.gameObject;
-                    // }
-
                     var hitCoordinate = hit.textureCoord;
-                    heightMapUpdate.SetFloat(SmoothMultiplier, quizItem.hardness / 5f);
+                    heightMapUpdate.SetFloat(SmoothMultiplier, quizItem.hardness / hardnessMultiplier);
                     heightMapUpdate.SetVector(ContactPosition, hitCoordinate);
                 }
             }
         }
-
-        // private void Update()
-        // {
-            // if (Input.GetMouseButton(0))
-            // {
-                // var ray = _mainCamera.ScreenPointToRay(Input.mousePosition);
-
-                // if (Physics.Raycast(ray, out var hit))
-                // {
-                    // var hitCoordinate = hit.textureCoord;
-                    // heightMapUpdate.SetVector(ContactPosition, hitCoordinate);
-                // }
-            // }
-        // }
     }
 }
